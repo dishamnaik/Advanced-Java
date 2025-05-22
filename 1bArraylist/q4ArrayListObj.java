d1f4fd12d24461a2b34efdc5ae1a91e23e9143a6
dishamnaik <153185215+dishamnaik@users.noreply.github.com>
2025-05-23 00:01:42 +0530

    Add files via upload

---
+++ 1bArraylist/q4ArrayListObj.java
@@ -0,0 +1,30 @@
+/*4. Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj)*/
+
+package assignment;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class q4ArrayListObj {
+
+ public static void main(String[] args) {
+     // Create an ArrayList of colors
+     ArrayList<String> colors = new ArrayList<>();
+
+     // Add colors to the ArrayList
+     colors.add("Blue");
+     colors.add("Red");
+     colors.add("Green");
+     colors.add("Yellow");
+     colors.add("Black");
+
+     // Print the original list
+     System.out.println("Original Color List: " + colors);
+
+     // Sort the ArrayList
+     Collections.sort(colors);
+
+     // Print the sorted list
+     System.out.println("Sorted Color List: " + colors);
+ }
+}
